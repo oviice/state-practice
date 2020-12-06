f26c30991f35cabf2e1ae43ab10b61e5577cb788
Ovi <oviice@gmail.com>
2020-12-06 13:27:32 +0600

    state

--- src/mycomponents/TestState.js
+++ src/mycomponents/TestState.js
@@ -9,14 +9,17 @@ class TestState extends React.Component {
   }
 
   changeMsg(a) {
+      a===1?console.log("das"):console.log("asdasd")
+      
     if (a === 1) {
-      this.setState({ message: "Button 1 pressed", time: 21 });
+      this.setState({ message: "Button 1 pressed", time: 21 ,login:true});
     } else {
-      this.setState({ message: "Button 2 pressed" ,time:24});
+      this.setState({ message: "Button 2 pressed" ,time:24,login:false});
     }
   }
   render() {
     return (
+        
       <div className="App">
         <button onClick={this.changeMsg.bind(this, 1)} style={{width:"100px" , margin: "40px"}}>button 1</button>
         <button onClick={this.changeMsg.bind(this, 2)}>button 2</button>
