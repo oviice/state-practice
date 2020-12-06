2dae5fa9eb2d65076768c9a867d6ed38f4c5563c
Ovi <oviice@gmail.com>
2020-12-06 13:12:31 +0600

    state

--- src/mycomponents/TestState.js
+++ src/mycomponents/TestState.js
@@ -18,7 +18,7 @@ class TestState extends React.Component {
   render() {
     return (
       <div className="App">
-        <button onClick={this.changeMsg.bind(this, 1)}>button 1</button>
+        <button onClick={this.changeMsg.bind(this, 1)} style={{width:"100px" , margin: "40px"}}>button 1</button>
         <button onClick={this.changeMsg.bind(this, 2)}>button 2</button>
         <h1>
           {this.state.message} {this.state.time}
